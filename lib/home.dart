4ff6b04fd5ebb116759f51fd8a74335b7b7b0c64
Abdullah Ridwan <53100755+abdullahridwan@users.noreply.github.com>
2024-02-14 18:35:37 -0500

    init

--- lib/home.dart
+++ lib/home.dart
@@ -15,87 +15,87 @@ class _HomeState extends State<Home> {
   final _controller = GlobalKey<PageFlipWidgetState>();
 
   List<dynamic> _poempages = [
-    // Container(
-    //   child: Stack(children: [
-    //     Container(
-    //       decoration: BoxDecoration(
-    //         image: DecorationImage(
-    //           image: AssetImage('cover.jpg'),
-    //           fit: BoxFit.cover,
-    //         ),
-    //       ),
-    //     ),
-    //     Container(
-    //       decoration: BoxDecoration(color: Colors.black.withOpacity(0.4)),
-    //     ),
-    //     Container(
-    //       child: Center(
-    //         child: Column(
-    //           mainAxisAlignment: MainAxisAlignment.center,
-    //           crossAxisAlignment: CrossAxisAlignment.center,
-    //           children: [
-    //             Text(
-    //               "Loving you through the Day",
-    //               style: TextStyle(
-    //                   color: Colors.white,
-    //                   fontSize: 40,
-    //                   fontWeight: FontWeight.w800,
-    //                   fontFamily: "Cutive"),
-    //             ),
-    //             SizedBox(
-    //               height: 5,
-    //             ),
-    //             Text(
-    //               "By your ridu",
-    //               style: TextStyle(
-    //                   color: Colors.white,
-    //                   fontSize: 20,
-    //                   fontWeight: FontWeight.w800,
-    //                   fontFamily: "Mullish"),
-    //             ),
-    //             SizedBox(
-    //               height: 10,
-    //             ),
-    //             Text(
-    //               "Swipe to the left to turn the page",
-    //               style: TextStyle(
-    //                   color: Colors.white,
-    //                   fontSize: 10,
-    //                   fontWeight: FontWeight.w800,
-    //                   fontFamily: "Mullish"),
-    //             ),
-    //           ],
-    //         ),
-    //       ),
-    //     ),
-    //   ]),
-    // ),
-    // PoemPage(
-    //     title: "My Valentine's to You",
-    //     colorWhite: false,
-    //     poemString:
-    //         "Dedicated to you, Michelly Welly Hassany, my love, forever and always.",
-    //     assetName: "flowers_red.gif",
-    //     labelText: "us hehe",
-    //     backgroundColor: Colors.purple.shade100),
-    // PoemPage(
-    //   backgroundColor: Colors.orange.shade600,
-    //   colorWhite: true,
-    //   title: "Dawn",
-    //   poemString:
-    //       "Everyday\nAs the sun rises\nI am grateful\nBecause I have been given another day\nTo be with you",
-    //   assetName: 'dawn.gif',
-    //   labelText: "petals across the sky",
-    // ),
-    // PoemPage(
-    //   backgroundColor: Colors.yellow.shade100,
-    //   title: "Sunshine",
-    //   colorWhite: false,
-    //   poemString:
-    //       "I love your smile\nYour Laugh\nAnd everything about you\nAs it warms my heart\nAnd brings me joy\n\nYou are my sunshine",
-    //   assetName: 'sunshine.gif',
-    //   labelText: "watering the bootiful sunflowers",
-    // ),
+    Container(
+      child: Stack(children: [
+        Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('cover.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(color: Colors.black.withOpacity(0.4)),
+        ),
+        Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Loving you through the Day",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w800,
+                      fontFamily: "Cutive"),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "By your ridu",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800,
+                      fontFamily: "Mullish"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Swipe to the left to turn the page",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w800,
+                      fontFamily: "Mullish"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    ),
+    PoemPage(
+        title: "My Valentine's to You",
+        colorWhite: false,
+        poemString:
+            "Dedicated to you, Michelly Welly Hassany, my love, forever and always.",
+        assetName: "flowers_red.gif",
+        labelText: "us hehe",
+        backgroundColor: Colors.purple.shade100),
+    PoemPage(
+      backgroundColor: Colors.orange.shade600,
+      colorWhite: true,
+      title: "Dawn",
+      poemString:
+          "Everyday\nAs the sun rises\nI am grateful\nBecause I have been given another day\nTo be with you",
+      assetName: 'dawn.gif',
+      labelText: "petals across the sky",
+    ),
+    PoemPage(
+      backgroundColor: Colors.yellow.shade100,
+      title: "Sunshine",
+      colorWhite: false,
+      poemString:
+          "I love your smile\nYour Laugh\nAnd everything about you\nAs it warms my heart\nAnd brings me joy\n\nYou are my sunshine",
+      assetName: 'sunshine.gif',
+      labelText: "watering the bootiful sunflowers",
+    ),
     PoemPage(
       backgroundColor: Color(0xFFEEA5A6),
       title: "A kiss in the sunset",
